ab5e004f8990ead4d27425fb6c8a2a18f2c491f2
PriyanshuKumarSinha <priyanshuk9066@gmail.com>
2022-08-22 17:26:19 +0530

    Program 22 : first n even natural numbers in reverse

---
+++ 22_firstNevenNaturalNumbersinRevWrecursion.c
@@ -0,0 +1,26 @@
+// print first 10 even natural numbers with recursion in reverse
+
+#include <stdio.h>
+
+//function declaration
+void printNnumbers(int);
+
+int main()
+{
+    int n; //using local n
+
+    printf("Enter a Number : ");
+    scanf("%d", &n);
+    printNnumbers(n); //function call
+    
+    printf("\n");
+    return 0;
+}
+
+//function definition
+void printNnumbers(int x)
+{
+    printf("%d ", 2*x); //a little change in logic  
+    if(x>1)
+        printNnumbers(x-1); //recursion
+}
