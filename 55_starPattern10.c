78cc2b1c609bc97c43eebf46d131974808bc4021
priyanshukumarsinha <priyanshuk9066@gmail.com>
2022-08-29 21:16:20 +0530

    Program 55 : Star Pattern 10

---
+++ 55_starPattern10.c
@@ -0,0 +1,32 @@
+// Star Pattern : 5
+//Author : Priyanshu Kumar Sinha // Date : 29 Aug 2022
+
+
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+
+#include<stdio.h>
+
+int main()
+{
+    int i, j,m=7,n=m/2+1,k=0; // n=4
+    for (i=0; i<m;i++) // 7rows
+    {
+        for (j=1; j<=n*2-1;j++) // 7 columns
+        {
+            if(j<n-k || j>n+k)  //1<4-5 || 1>4+5
+                printf(" ");
+            else
+                printf("*");
+        }
+        printf("\n");
+        
+        if(i<n-1)
+            k++;
+        else
+            k--;
+    }
+}
