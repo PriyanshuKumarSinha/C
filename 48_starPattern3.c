b3352e25dedf01dfc749bc1e81b7ffd96ac86e75
priyanshukumarsinha <priyanshuk9066@gmail.com>
2022-08-29 18:55:51 +0530

    Program 48 : Star Pattern 3

---
+++ 48_starPattern3.c
@@ -0,0 +1,26 @@
+// Star Pattern : 3
+//Author : Priyanshu Kumar Sinha // Date : 29 Aug 2022
+
+// *****
+//  ****
+//   ***
+//    **
+//     *
+
+#include<stdio.h>
+
+int main()
+{
+    int i, j;
+    for (i=0; i<5;i++)
+    {
+        for (j=1; j<=5;j++)
+        {
+            if(j>i)
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+}
