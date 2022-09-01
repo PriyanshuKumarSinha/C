e36f3a2b2c42905c655c996cf27adbe7a48d2381
priyanshukumarsinha <priyanshuk9066@gmail.com>
2022-09-01 16:20:43 +0530

    Program 63 : Star Pattern 18

---
+++ 63_starPattern18.c
@@ -0,0 +1,26 @@
+// Star Pattern : 17
+//Author : Priyanshu Kumar Sinha // Date : 1 Aug 2022
+
+//*********     //123456789
+//_*******_     //_2345678_
+//__*****__     //__34567__
+//___***___     //___456___
+//____*____     //____5____
+
+#include<stdio.h>
+
+int main()
+{
+    int i,j,n=5,m=n*2-1;
+    for(i=1; i<=n;i++)    
+    {
+        for (j=1;j<=m;j++)
+        {
+            if(j>=i && j<=m-i+1)
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+}
