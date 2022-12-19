652c663d638368e82ea959276aee8578a77ccea5
Priyanshu Kumar Sinha <79042642+priyanshukumarsinha@users.noreply.github.com>
2022-12-19 22:35:43 +0530

    Create 82_areaOfTriangle.c

---
+++ 82_areaOfTriangle.c
@@ -0,0 +1,13 @@
+//Author : Priyanshu Kumar Sinha
+//Date : 19/12/2022
+//Program to calculate area of a triangle (using 1/2 * base * height)
+
+#include <stdio.h>
+int main()
+{
+  float b=10.5,h=11.5,area;
+  area = 0.5*b*h;
+  printf("Area of Triangle = %d", area);
+  
+  return 0;
+}
