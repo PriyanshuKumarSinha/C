9eb504d3a97e386cdde9df66d9a1728664914727
Priyanshu Kumar Sinha <79042642+priyanshukumarsinha@users.noreply.github.com>
2022-12-19 22:42:47 +0530

    Program 84 : Average of Three Numbers

---
+++ 84_avgOf3.c
@@ -0,0 +1,11 @@
+//Author : Priyanshu Kumar Sinha
+//Date : 19/12/2022
+//Program to Caluculate average of threee numbers
+
+#include <stdio.h>
+int main()
+{
+  int a, b, c;
+  printf("Average of three number %d, %d and %d is %d", a, b, c, (a+b+c)/3);
+  return 0;
+}
