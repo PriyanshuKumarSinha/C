71aa6069594f007ff2550f1cc883eb21a05dde4d
Priyanshu Kumar Sinha <79042642+priyanshukumarsinha@users.noreply.github.com>
2022-12-19 22:39:47 +0530

    Program 82 : Square and Cube of a number

---
+++ 83_square&cube.c
@@ -0,0 +1,12 @@
+//Author : Priyanshu Kumar Sinha
+//Date : 19/12/2022
+//Program to calculate square and cube of a number
+
+#include <stdio.h>
+int main()
+{
+  int a=5;
+  printf("Square of %d is %d", a,a*a);
+  printf("\nCube of %d is %d", a , a*a*a);
+  return 0;
+}
